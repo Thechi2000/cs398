0b9ce3e5bbcf1d47cfbd9acb429d96031a202644
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-02 18:05:51 +0100

    feat: add global event bus for frontend

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,29 +1,27 @@
 import * as Monaco from "@monaco-editor/react";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
+import { listenEvent, useEventBus } from "../main";
 
-function FileTab(props: {
-  name: string;
-  selected: boolean;
-  select: () => void;
-  close: () => void;
-}) {
+function FileTab(props: { path: string; selected: boolean }) {
+  const events = useEventBus();
+
   return (
     <div
       className={`cursor-pointer px-2 text-lg flex items-center gap-2 ${
         props.selected ? "bg-white" : "bg-gray-200"
       }`}
-      key={props.name}
-      onClick={props.select}
+      key={props.path}
+      onClick={() => events.emit("editor.tab.select", props.path)}
     >
       <Cross2Icon
         className="hover:scale-125"
         onClick={(e) => {
           e.stopPropagation();
-          props.close();
+          events.emit("editor.tab.close", props.path);
         }}
       />
-      <p>{props.name}</p>
+      <p>{props.path}</p>
     </div>
   );
 }
@@ -50,14 +48,20 @@ export function Editor() {
     }
   }, [currentFile, models]);
 
+  listenEvent("editor.tab.select", setCurrentFile);
+  listenEvent("editor.tab.close", closeFile, [models, currentFile]);
+
   function closeFile(name: string) {
     let newModels = { ...models };
     delete newModels[name];
+    console.log("soeht");
+    console.log(name + "; " + currentFile);
 
     // If the current file is the one to be closed, we set the previous one (if none, the next) as the current.
     if (name === currentFile) {
       let newIndex = Object.keys(models).indexOf(name) - 1;
       if (newIndex < 0) newIndex = 0;
+      console.log(newIndex);
       setCurrentFile(Object.keys(newModels)[newIndex]);
     }
 
@@ -68,12 +72,7 @@ export function Editor() {
     <div className="w-full h-full flex flex-col">
       <div className="flex select-none">
         {Object.keys(models).map((path) => (
-          <FileTab
-            name={path}
-            selected={path === currentFile}
-            select={() => setCurrentFile(path)}
-            close={() => closeFile(path)}
-          />
+          <FileTab path={path} selected={path === currentFile} />
         ))}
       </div>
       <Monaco.Editor className="grow" defaultLanguage="verilog" />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import { EventEmitter } from "@tauri-apps/api/shell";
+
+const EventBusContext = React.createContext(new EventEmitter());
+export const useEventBus = () => useContext(EventBusContext);
+export const listenEvent = (
+  name: string,
+  handler: (...args: any[]) => void,
+  dependencies?: any[]
+) => {
+  const events = useEventBus();
+  useEffect(() => {
+    events.on(name, handler);
+    return () => {
+      events.off(name, handler);
+    };
+  }, dependencies || []);
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <EventBusContext.Provider value={new EventEmitter()}>
+      <App />
+    </EventBusContext.Provider>
+  </React.StrictMode>
 );
