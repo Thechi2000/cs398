1b1b394de4a9fcbb2f7b3071b42c741c23ff5a0d
Roman Paccaud <roman.paccaud@epfl.ch>
2023-11-16 13:32:05 +0100

    feat: add read_dir command

--- src-tauri/src/project.rs
+++ src-tauri/src/project.rs
@@ -2,27 +2,81 @@
 Project state
  */
 
-use std::{ffi::OsString, path::PathBuf};
+use std::{
+    ffi::OsString,
+    fs::{self, DirEntry, FileType},
+    path::PathBuf,
+};
+
+use crate::{error::Error, state::State, util::to_utf8};
 
 pub struct Project {
     /// Project name
     pub name: String,
     /// Directory to compile
-    pub project_directory: OsString,
+    pub project_directory: String,
     /// Files to not compile
-    pub excluded_files: Vec<OsString>,
+    pub excluded_files: Vec<String>, //TODO
+}
+
+pub struct Directory {
+    /// Absolute Path to the directory
+    pub path: String,
+    /// File Name
+    pub name: String,
+    /// Empty : the directory is a file / Non-empty : Files and directories embeded in it
+    pub children: Vec<Directory>,
+}
+#[tauri::command]
+async fn get_project_directory_tree(state: tauri::State<'_, State>) -> Result<Directory, Error> {
+    if let Some(project) = state.project() {
+        project.read_dir()
+    } else {
+        Err(Error::NoProject)
+    }
 }
 
 impl Project {
-    pub fn from_current_dir() -> Result<Self, std::io::Error> {
-        Ok(Self::from_dir(std::env::current_dir()?))
+    pub fn from_current_dir() -> Result<Self, Error> {
+        Ok(Self::from_dir(to_utf8(&std::env::current_dir()?)?))
     }
 
-    pub fn from_dir(path: PathBuf) -> Self {
+    pub fn from_dir(path: String) -> Self {
         Project {
-            name: path.file_name().unwrap().to_string_lossy().to_string(),
-            project_directory: path.into_os_string(),
+            name: PathBuf::from(&path)
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .to_string(),
+            project_directory: path,
             excluded_files: vec![],
         }
+    }
+
+    pub fn read_dir(&self) -> Result<Directory, Error> {
+        fn recursive_read_dir<I: Iterator<Item = Result<DirEntry, std::io::Error>>>(
+            it: I,
+        ) -> Result<Vec<Directory>, Error> {
+            let mut project_tree = vec![];
+            for entry in it {
+                let entry = entry?;
+                project_tree.push(Directory {
+                    path: to_utf8(&entry.path())?,
+                    name: to_utf8(&entry.file_name())?,
+                    children: if entry.file_type()?.is_dir() {
+                        recursive_read_dir(fs::read_dir(entry.path())?)?
+                    } else {
+                        vec![]
+                    },
+                })
+            }
+            Ok(project_tree)
+        }
+
+        Ok(Directory {
+            path: to_utf8(&self.project_directory)?,
+            name: to_utf8(&self.name)?,
+            children: recursive_read_dir(fs::read_dir(&self.project_directory)?)?,
+        })
     }
 }
