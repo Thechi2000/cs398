d574667cfab1850c606e3dc802ced634a8cf6cfd
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-12-11 21:22:24 +0100

    feat: make components resizable

--- src/components/MenuScreen.tsx
+++ src/components/MenuScreen.tsx
@@ -1,3 +1,4 @@
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { Editor } from "./Editor";
 import FileExplorer from "./FileExplorer";
 import Output from "./Output";
@@ -5,17 +6,23 @@ import Output from "./Output";
 export default function MenuScreen() {
   return (
     <div id="app">
-      <div className="flex h-2/3">
-        <div className="bg-amber-200 w-[20%] h-full overflow-scroll overflow-y-hidden">
-          <FileExplorer />
-        </div>
-        <div className="bg-amber-400 w-[80%] h-full">
-          <Editor />
-        </div>
-      </div>
-      <div className="bg-amber-600 h-1/3">
-        <Output />
-      </div>
+      <PanelGroup direction="vertical">
+        <Panel>
+          <PanelGroup direction="horizontal">
+            <Panel defaultSizePercentage={25}>
+              <FileExplorer />
+            </Panel>
+            <PanelResizeHandle />
+            <Panel>
+              <Editor />
+            </Panel>
+          </PanelGroup>
+        </Panel>
+        <PanelResizeHandle />
+        <Panel defaultSizePercentage={30}>
+          <Output />
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
