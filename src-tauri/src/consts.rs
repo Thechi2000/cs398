390c54c880d0cbefb4a1ba9c787032bd395adf8e
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-12-19 10:03:33 +0100

    feat: add binaries for apple

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -10,5 +10,17 @@ mod inner {
     pub const VVP_EXE: &str = "../binaries/x86_64-pc-windows/bin/vvp.exe";
 }
 
+#[cfg(all(target_os = "darwin", target_arch = "aarch64"))]
+mod inner {
+    pub const IVERILOG_EXE: &str = "../binaries/aarch64-apple-darwin/bin/iverilog";
+    pub const VVP_EXE: &str = "../binaries/aarch64-apple-darwin/bin/vvp";
+}
+
+#[cfg(all(target_os = "darwin", target_arch = "x86_64"))]
+mod inner {
+    pub const IVERILOG_EXE: &str = "../binaries/x86_64-apple-darwin/bin/iverilog";
+    pub const VVP_EXE: &str = "../binaries/x86_64-apple-darwin/bin/vvp";
+}
+
 // The use of a submodule allows to put all the os-dependent variables behind a single `#[cfg]` attribute.
 pub use inner::*;
