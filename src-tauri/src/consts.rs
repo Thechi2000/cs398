043536fc544dd0ec0763dbdb804690e2bc009c00
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-01-11 17:24:06 +0100

    feat: setup 0.1.0

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -10,13 +10,13 @@ mod inner {
     pub const VVP_EXE: &str = "../binaries/x86_64-pc-windows/bin/vvp.exe";
 }
 
-#[cfg(all(target_os = "darwin", target_arch = "aarch64"))]
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 mod inner {
     pub const IVERILOG_EXE: &str = "../binaries/aarch64-apple-darwin/bin/iverilog";
     pub const VVP_EXE: &str = "../binaries/aarch64-apple-darwin/bin/vvp";
 }
 
-#[cfg(all(target_os = "darwin", target_arch = "x86_64"))]
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
 mod inner {
     pub const IVERILOG_EXE: &str = "../binaries/x86_64-apple-darwin/bin/iverilog";
     pub const VVP_EXE: &str = "../binaries/x86_64-apple-darwin/bin/vvp";
